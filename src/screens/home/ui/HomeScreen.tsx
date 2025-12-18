62c3d8eda724e48855ca6645688220be017492c6
kim1350 <rholmogorov12@gmail.com>
2025-12-18 23:05:25 +0500

    feat(shared): variables

--- src/screens/home/ui/HomeScreen.tsx
+++ src/screens/home/ui/HomeScreen.tsx
@@ -3,29 +3,12 @@ import React, { useState } from "react";
 import { ImageBackground } from "expo-image";
 import { useThemeColors } from "@/shared/hooks";
 import { GlassView } from "expo-glass-effect";
-import { scaleW } from "@/shared/constants";
+import { EXAMPLE_WALLETS, scaleW } from "@/shared/constants";
 import { DropdownWallet, type Option } from "@/widgets/dropdownWallet";
-
-const exapleoptions = [
-  {
-    title: "USDT Wallet",
-    address: "0x1234567890abcdef1234567890abcdef12345678",
-    amount: "1500",
-    currency: "USDT",
-    value: "1",
-  },
-  {
-    title: "ETH Wallet",
-    address: "0x1234567890abcdef1234567890abcdef12345678",
-    amount: "2.5",
-    currency: "ETH",
-    value: "2",
-  },
-];
 
 export const HomeScreen = () => {
   const colors = useThemeColors();
-  const [selected, setSelected] = useState<Option>(exapleoptions[0]);
+  const [selected, setSelected] = useState<Option>(EXAMPLE_WALLETS[0]);
 
   return (
     <ImageBackground
@@ -52,7 +35,7 @@ export const HomeScreen = () => {
             onSelect={(item) => {
               setSelected(item);
             }}
-            options={exapleoptions}
+            options={EXAMPLE_WALLETS}
           ></DropdownWallet>
         </GlassView>
       </View>
---
+++ src/shared/constants/variables.ts
@@ -0,0 +1,30 @@
+export const EXAMPLE_WALLETS = [
+  {
+    title: "USDT Wallet",
+    address: "0x1234567890abcdef1234567890abcdef12345678",
+    amount: "1500",
+    currency: "USDT",
+    value: "1",
+  },
+  {
+    title: "ETH Wallet",
+    address: "0x1234567890abcdef1234567890abcde00000000X",
+    amount: "2.5",
+    currency: "ETH",
+    value: "2",
+  },
+  {
+    title: "ETHD Wallet",
+    address: "0x1234567890abcdef1234567890abcdef12YYYY66",
+    amount: "2.5",
+    currency: "ETHD",
+    value: "3",
+  },
+  {
+    title: "THD Wallet",
+    address: "0x1234567890abcdef1234567890abcdef12YYYY66",
+    amount: "2.5",
+    currency: "ETHD",
+    value: "4",
+  },
+];
