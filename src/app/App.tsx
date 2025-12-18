215ac49653f3a25d5297a54c1b9a44ebf5d2ac36
kim1350 <rholmogorov12@gmail.com>
2025-12-18 13:20:12 +0500

    feat(fonts): add fonts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { Navigator } from "./navigation";
 import { CustomThemeProvider, SafeAreaProvider } from "./providers";
-
+import "react-native-reanimated";
 export default function App() {
   return (
     <SafeAreaProvider>
--- src/app/navigation/Navigator.tsx
+++ src/app/navigation/Navigator.tsx
@@ -3,6 +3,8 @@ import { createStaticNavigation } from "@react-navigation/native";
 import * as SplashScreen from "expo-splash-screen";
 import { useThemeContext } from "../providers";
 import { RootStack } from "./RootStack";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -10,6 +12,20 @@ const Navigation = createStaticNavigation(RootStack);
 
 export const Navigator = () => {
   const { theme } = useThemeContext();
+  // Use `useFonts` only if you can't use the config plugin.
+  const [loaded, error] = useFonts({
+    Montserrat: require("@assets/fonts/Montserrat-VariableFont.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
 
   return (
     <Navigation
