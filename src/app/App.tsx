71da844e4e1297d5415830e9e258eaf846b438a5
kim1350 <rholmogorov12@gmail.com>
2025-12-19 11:38:27 +0500

    feat(provider): add KeyboardProvider and reacthookform

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,18 @@
 import { Navigator } from "./navigation";
-import { CustomThemeProvider, SafeAreaProvider } from "./providers";
+import {
+  CustomThemeProvider,
+  KeyboardProvider,
+  SafeAreaProvider,
+} from "./providers";
 import "react-native-reanimated";
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <CustomThemeProvider>
-        <Navigator />
-      </CustomThemeProvider>
-    </SafeAreaProvider>
+    <KeyboardProvider>
+      <SafeAreaProvider>
+        <CustomThemeProvider>
+          <Navigator />
+        </CustomThemeProvider>
+      </SafeAreaProvider>
+    </KeyboardProvider>
   );
 }
